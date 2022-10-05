1c0623b602f4fc1e0c567e76045b776738d2e7c0
Moises Cordero <moisescordero@Moisess-MacBook-Pro.local>
2022-10-05 01:56:37 -0500

    Models

--- rent/models.py
+++ rent/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    number = models.SmallIntegerField(unique=True, null=False)
+    capacity = models.SmallIntegerField(null=False)
+
+
+class Event(models.Model):
+    TYPE_CHOICES = [('public', 'public'), ('private', 'private')]
+    type = models.CharField(max_length=10, choices=TYPE_CHOICES, null=False)
+    name = models.CharField(max_length=100, unique=True, null=False)
+    room_number = models.SmallIntegerField(null=False)
+    room = models.OneToOneField('Room', on_delete=models.PROTECT, null=False)
+
+
+class Booking(models.Model):
+    customer = models.CharField(max_length=50, null=False)
+    event_name = models.CharField(max_length=100, null=False)
+    event = models.ForeignKey('Event', on_delete=models.PROTECT, null=False)
+
+    class Meta:
+        unique_together = ['customer', 'event']
