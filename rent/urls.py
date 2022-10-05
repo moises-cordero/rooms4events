a8e02c6843ca46387bd9a15ad5261e7ce24e4b6d
Moises Cordero <moisescordero@Moisess-MacBook-Pro.local>
2022-10-05 13:49:40 -0500

    Fix routing naming

--- rent/urls.py
+++ rent/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
-from rent.views import CreateRoomView, DeleteRoomView, CreateEventView, BookingView
-from rent.views import ListPublicEventsView, DeleteBookingView
+from rent.views import CreateRoomView, DeleteRoomView, CreateEventView, CreateBookingView
+from rent.views import ListEventsAvailableView, DeleteBookingView
 
 
 urlpatterns = [
-    path('room', CreateRoomView.as_view(), name='room'),
-    path('room/<int:number>', DeleteRoomView.as_view(), name='room'),
-    path('event', CreateEventView.as_view(), name='event'),
-    path('events', ListPublicEventsView.as_view(), name='events'),
-    path('booking', BookingView.as_view(), name='booking'),
-    path('booking/<str:customer>/<str:event_name>', DeleteBookingView.as_view(), name='booking'),
+    path('rooms', CreateRoomView.as_view(), name='create-room'),
+    path('rooms/<int:number>', DeleteRoomView.as_view(), name='delete-room'),
+    path('events', CreateEventView.as_view(), name='create-event'),
+    path('events/available', ListEventsAvailableView.as_view(), name='events-available'),
+    path('bookings', CreateBookingView.as_view(), name='create-booking'),
+    path('bookings/<str:customer>/<str:event_name>', DeleteBookingView.as_view(),
+         name='delete-booking'
+         ),
 ]
