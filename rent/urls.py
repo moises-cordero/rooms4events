81278f6529b846daa0a73baf90c8dd444fd6b01b
Moises Cordero <moisescordero@Moisess-MacBook-Pro.local>
2022-10-05 02:01:04 -0500

    Settings and urls

---
+++ rent/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from rent.views import CreateRoomView, DeleteRoomView, CreateEventView, BookingView
+from rent.views import ListPublicEventsView, DeleteBookingView
+
+
+urlpatterns = [
+    path('room', CreateRoomView.as_view(), name='room'),
+    path('room/<int:number>', DeleteRoomView.as_view(), name='room'),
+    path('event', CreateEventView.as_view(), name='event'),
+    path('events', ListPublicEventsView.as_view(), name='events'),
+    path('booking', BookingView.as_view(), name='booking'),
+    path('booking/<str:customer>', DeleteBookingView.as_view(), name='booking'),
+]
